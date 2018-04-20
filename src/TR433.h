93b41e07f3d7eb164ab74474a8031abd4e2b5520
Alex Fedorov <fedorov1984@gmail.com>
2018-04-19 21:33:09 -0700

    create TR433

--- src/TR433.h
+++ src/TR433.h
@@ -11,6 +11,7 @@
 #include <Receiver.h>
 #include <HardwareSerial.h>
 #include <Arduino.h>
+#include <types.h>
 
 template<typename Content>
 class TR433
@@ -20,35 +21,9 @@ public:
 
     TR433();
 
-    mark_os::commons::Optional <Message<Content>> receive() override;
+    mark_os::commons::Optional<Message<Content>> receive() override;
 
-    void send(Message <Content> &message) override;
+    void send(Message<Content> &message) override;
 };
-
-template<typename Content>
-void TR433<Content>::send(Message <Content> &message) {
-    driver.send((uint8_t *) &message, sizeof(message));
-    driver.waitPacketSent();
-}
-
-template<typename Content>
-TR433<Content>::TR433() {
-    if (!driver.init()) {
-        Serial.println("init failed");
-    }
-}
-
-template<typename Content>
-mark_os::commons::Optional <Message<Content>> TR433<Content>::receive() {
-    Message <Content> message;
-
-    uint8_t buflen = sizeof(message);
-
-    if (driver.recv((uint8_t *) &message, &buflen)) {
-        return mark_os::commons::optional(message);
-    }
-    return mark_os::commons::none < Message < Content >> ();
-}
-
 
 #endif //MARKOS_ARDUINO_CONTROLLER_TRANSCEIVER430_H
